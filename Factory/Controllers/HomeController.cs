0ac47b71862bd7863f20b8b4b84a1114c1ee0975
Joe W <whobbes22@gmail.com>
2023-05-26 09:09:25 -0700

    Linked up SQL database with migrations

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,28 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
-using TopTenFilmList.Models;
+using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace TopTenFilmList.Controllers
+namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
-      private readonly TopTenFilmListContext _db;
+      private readonly FactoryContext _db;
 
-      public HomeController(TopTenFilmListContext db)
+      public HomeController(FactoryContext db)
       {
+
         _db = db;
       }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        Film[] films = _db.Films.ToArray();
-        Actor[] actors = _db.Actors.ToArray();
-        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
-        model.Add("films", films);
-        model.Add("actors", actors);
-        return View(model);
+        // Film[] films = _db.Films.ToArray();
+        // Actor[] actors = _db.Actors.ToArray();
+        // Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        // model.Add("films", films);
+        // model.Add("actors", actors);
+        // return View(model);
+        return View();
       }
     }
 }
