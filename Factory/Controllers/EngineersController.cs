8b0d53e801a9603873476b73c067910e7ef07259
Joe W <whobbes22@gmail.com>
2023-05-26 14:15:27 -0700

    added Engineerscontroller

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -17,7 +17,109 @@ namespace Factory.Controllers
 
     public ActionResult Index()
     {
+      return View(_db.Engineers.ToList());
+    }
+
+    
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+                .Include(engineer => engineer.JoinEntities)
+                .ThenInclude(join => join.Machine)
+                .FirstOrDefault(engineer => engineer.EngineerId == id);
+      // ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisEngineer);
+    }
+
+
+    public ActionResult Create()
+    {
+     
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      if(!ModelState.IsValid)
+      {
+        return View(engineer);
+      } 
+      else
+      {
+        _db.Engineers.Add(engineer);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult AddMachine(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View(thisEngineer);
+      
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int machineId)
+    {
+      #nullable enable
+      MachineEngineer? joinEntity = _db.MachineEngineers.FirstOrDefault(join => (join.MachineId == machineId && join.EngineerId == engineer.EngineerId));
+      #nullable disable
+      if( joinEntity == null && machineId !=0)
+      {
+        _db.MachineEngineers.Add(new MachineEngineer() {MachineId = machineId, EngineerId = engineer.EngineerId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = engineer.EngineerId});
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      _db.Engineers.Remove(thisEngineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      MachineEngineer joinEntry = _db.MachineEngineers.FirstOrDefault(entry => entry.MachineEngineerId == joinId);
+      _db.MachineEngineers.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer)
+    {
+      if(!ModelState.IsValid)
+      {
+        return View(engineer);
+      }
+      else 
+      {
+        _db.Engineers.Update(engineer);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+    }
+    
   }
 }
